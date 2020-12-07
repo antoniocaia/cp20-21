7924feacef71a60af2b5afbdc493a64f51a27b1c
Antonio Caia <antonio.caia96@gmail.com>
2020-12-07 23:15:31 +0100

    Fix Frogs and Mosq
    Wrong answer test 5

--- code/frogs_and_mosquitoes.cpp
+++ code/frogs_and_mosquitoes.cpp
@@ -11,11 +11,17 @@ struct frog {
 	int64_t tongue;
 	int mos_count;
 
+	int64_t l;
+	int64_t r;
+
 	frog() {
-		id = INT64_MAX;
+		id = -1;
 		pos = -1;
 		tongue = -1;
 		mos_count = -1;
+
+		l = 0;
+		r = 0;
 	}
 
 	frog(int i, int64_t x, int64_t t) {
@@ -23,6 +29,9 @@ struct frog {
 		pos = x;
 		tongue = t;
 		mos_count = 0;
+
+		l = 0;
+		r = 0;
 	}
 
 	void update_frog(long s) {
@@ -51,38 +60,51 @@ bool frog_by_id(frog a, frog b) {
 
 
 // Insert
-void insert_frog(std::vector<frog>& frogs, frog& f) {
-	int i = 0;
-	frog node = frogs[i];
-	while (node.id != INT64_MAX) {
-		if (f.pos < node.pos)
-			node = frogs[i = i * 2 + 1];
-		else
-			node = frogs[i = i * 2 + 2];
+void insert_frog(std::vector<frog>& frogs, frog& f, int64_t v_i) {
+	frog node = frogs[1];
+	int64_t crr = 1;
+	int64_t prc = -1;
+
+	while (node.id != -1) {
+		prc = crr;
+		if (f.pos < node.pos) {
+			crr = node.l;
+			node = frogs[node.l];
+		}
+		else {
+			crr = node.r;
+			node = frogs[node.r];
+		}
 	}
-	frogs[i] = f;
-	//int i_prec = (i + 1) / 2 - 1;
+	frogs[v_i] = f;
+
+	if (prc != -1) {
+		if (frogs[prc].pos < f.pos)
+			frogs[prc].r = v_i;
+		else
+			frogs[prc].l = v_i;
+	}
 }
 
-void find_frog(std::vector<frog> frogs, int i, int m_pos, int& f_i) {
+void find_frog(std::vector<frog> &frogs, int i, int m_pos, int& f_i) {
 	// No more frogs to check, end
-	if (frogs[i].id == INT64_MAX) return;
+	if (frogs[i].id == -1) return;
 
 	// Check if current frog is best candidate
 	bool can_eat = frogs[i].pos <= m_pos && frogs[i].pos + frogs[i].tongue >= m_pos;
-	bool most_left = f_i != -1 || frogs[i].pos < frogs[f_i].pos;
+	bool most_left = f_i == -1 || frogs[i].pos < frogs[f_i].pos;
 
 
 	if (can_eat && most_left) { 					// If the frog match the requisites update index and check for leftmost frogs
 		f_i = i;
-		find_frog(frogs, i * 2 + 1, m_pos, f_i);
+		find_frog(frogs, frogs[i].l, m_pos, f_i);
 	}
 	else if (m_pos < frogs[i].pos) {				// If the mosq is to the left size of the current frog, check only leftmost frogs
-		find_frog(frogs, i * 2 + 1, m_pos, f_i);
+		find_frog(frogs, frogs[i].l, m_pos, f_i);
 	}
 	else {											// General case
-		find_frog(frogs, i * 2 + 1, m_pos, f_i);
-		find_frog(frogs, i * 2 + 2, m_pos, f_i);
+		find_frog(frogs, frogs[i].l, m_pos, f_i);
+		find_frog(frogs, frogs[i].r, m_pos, f_i);
 	}
 }
 
@@ -92,14 +114,15 @@ int main()
 	int f_n, m_n;
 	std::cin >> f_n >> m_n;
 
-	std::vector<frog> frogs(pow(2, f_n) + 1);
+	std::vector<frog> frogs(f_n + 1);
+	int64_t free_pos = 1;
 
 	for (int i = 0; i < f_n; i++) {
 		int64_t x, t;
 		std::cin >> x >> t;
 
 		struct frog f = { i, x, t };
-		insert_frog(frogs, f);
+		insert_frog(frogs, f, free_pos++);
 	}
 
 	std::map<int64_t, int64_t> mosqs;
@@ -108,7 +131,7 @@ int main()
 		std::cin >> x >> s;
 
 		int f_eat = -1;
-		find_frog(frogs, 0, x, f_eat);
+		find_frog(frogs, 1, x, f_eat);
 
 		if (f_eat != -1) {
 			// Frog f_eat can eat
@@ -125,7 +148,9 @@ int main()
 	// sort before output
 	sort(frogs.begin(), frogs.end(), [](frog a, frog b) { return a.id < b.id; });
 
-	for (int i = 0; i < f_n; i++) {
+	for (int i = 1; i < f_n + 1; i++) {
 		std::cout << frogs[i].mos_count << " " << frogs[i].tongue << "\n";
 	}
+
+	return 0;
 }
