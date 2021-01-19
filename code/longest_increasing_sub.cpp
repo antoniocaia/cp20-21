1e4bd14a7298ca5fea6b6477089813e1a93f2fd5
Antonio Caia <antonio.caia96@gmail.com>
2021-01-19 14:19:42 +0100

    Solved longest increasing sub in n log n time
    Add description

--- code/longest_increasing_sub.cpp
+++ code/longest_increasing_sub.cpp
@@ -1,38 +1,12 @@
-/*
-// { Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-int longestSubsequence(int, int[]);
-
-int main()
-{
-	//taking total testcases
-	int t, n;
-	cin >> t;
-	while (t--)
-	{
-		//taking size of array
-		cin >> n;
-		int a[n];
-
-		//inserting elements to the array
-		for (int i = 0;i < n;i++)
-			cin >> a[i];
-
-		//calling method longestSubsequence()
-		cout << longestSubsequence(n, a) << endl;
-	}
-}
-// } Driver Code Ends
-*/
+#include <iostream>
+#include <vector>
 
 /*
-Time: 0.29
-
+time complexity: O(n^2)
+space complexity: O(n)
 */
 
-int64_t check(int n, int a[], int i, std::vector<int64_t> &lis) {
+int64_t check(int a[], int i, std::vector<int64_t> &lis) {
 	int64_t max = 0;
 	for (int j = 0; j < i; j++) {
 		if (a[j] < a[i] && lis[j] > max) {
@@ -46,7 +20,7 @@ int longestSubsequence(int n, int a[])
 {
 	std::vector<int64_t> lis(n);
 	for (int i = 0; i < n; i++) {
-		lis[i] = check(n, a, i, lis);
+		lis[i] = check(a, i, lis);
 	}
 
 	// looks for max sol
