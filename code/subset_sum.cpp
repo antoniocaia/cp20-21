bf81759b4df0889c2eb0dcf76e53b575c46f495a
Antonio Caia <antonio.caia96@gmail.com>
2021-01-20 16:13:14 +0100

    Update subset sum code
    Added description

--- code/subset_sum.cpp
+++ code/subset_sum.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 #include <vector>
 
+/*
+time complexity: O(n * sum of elements)
+space complexity: O(n * sum of elements)
+
+First we divide the sum of the elements by two to
+find the expected value s that each subset has to
+reach. Then using a matrix of n + 1 rows and s + 1
+columns we check for each subset which values can
+be obtained. We start from the empty set, and for
+each new element in the set we need two controls:
+- if the element is bigger than the value that we
+want reach we check in the previuos subset if the 
+value is already present.
+- if the element is smaller than the value that we
+want reach we check in the previuos subset if the 
+value is already present or if it's possible to 
+obtain x such that x + element = value.
+*/
+
 class Solution {
 public:
 	int equalPartition(int N, int arr[])
@@ -25,9 +44,10 @@ public:
 			for (int j = 1; j <= sol; j++) {
 				if (j < arr[i - 1])
 					m[i][j] = m[i - 1][j];
-				if (j >= arr[i - 1])
+				else 	if (j >= arr[i - 1])
 					m[i][j] = m[i - 1][j] || m[i - 1][j - arr[i - 1]];
 			}
+			if (m[i][sol] == true) return true;		// As soon as we find a solution we can return true;
 		}
 
 		return m[N][sol];
