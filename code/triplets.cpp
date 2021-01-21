ee0b31686f0549f874b2261a4bdcba2b194a4120
Antonio Caia <antonio.caia96@gmail.com>
2021-01-21 18:09:56 +0100

    Update triplets

--- code/triplets.cpp
+++ code/triplets.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <vector>
 
+/*
+time complexity: O(n^2)
+space complexity: O(n)
+
+For each element x we count:
+- how many elements greater than x are present in its suffix
+- how many elements smaller than x are present in its prefix
+We then calculate the number of triplets counting for each
+element x all the combination assuming x as middle term.
+*/
+
 int main() {
 
 	int64_t n = 0;
@@ -9,7 +20,7 @@ int main() {
 	std::vector<int64_t> in;
 	in.reserve(n);
 
-	for (uint64_t i = 0; i < n; ++i) {
+	for (int64_t i = 0; i < n; ++i) {
 		int64_t x = 0;
 		std::cin >> x;
 		in.push_back(x);
@@ -18,12 +29,14 @@ int main() {
 	std::vector<int64_t> l(n);
 	std::vector<int64_t> r(n);
 
-	for (int i = 0; i < n - 1; ++i)
-		for (int j = i + 1; j < n; ++j)
+	for (int i = 0; i < n - 1; ++i) {
+		for (int j = i + 1; j < n; ++j) {
 			if (in[i] < in[j]) {
 				l[i]++;
 				r[j]++;
 			}
+		}
+	}
 
 	int64_t c = 0;
 
