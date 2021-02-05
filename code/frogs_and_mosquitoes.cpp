36523c381f9155b20bc6a49dbf09b5195d49f792
Antonio Caia <antonio.caia96@gmail.com>
2021-02-05 18:47:01 +0100

    Solved frog and mosq

--- code/frogs_and_mosquitoes.cpp
+++ code/frogs_and_mosquitoes.cpp
@@ -13,17 +13,11 @@ struct frog {
 	li tongue;
 	li mos_count;
 
-	li l; // left index for the bst
-	li r; // rigth index for the bst
-
 	frog() {
 		id = -1;
 		pos = -1;
 		tongue = -1;
 		mos_count = 0;
-
-		l = 0;
-		r = 0;
 	}
 
 	frog(li i, li x, li t) {
@@ -31,83 +25,75 @@ struct frog {
 		pos = x;
 		tongue = t;
 		mos_count = 0;
-
-		l = 0;
-		r = 0;
-	}
-
-	void update_frog(li s) {
-		mos_count++;
-		tongue += s;
-	}
-	// Check if frog can eat any of the stored mosq. The mosq are ordered in increasing position.
-	void feast(std::multimap<li, li>& ms) {
-		auto to_eat = ms.lower_bound(pos);
-		if (to_eat != ms.end() && to_eat->first >= pos && to_eat->first <= pos + tongue) {
-			update_frog(to_eat->second);
-			ms.erase(to_eat);
-			return feast(ms);
-		}
 	}
 };
 
 bool frog_by_id(frog a, frog b) { return a.id < b.id; }
 
-void insert_frog(std::vector<frog>& frogs, li id, li pos, li tongue, li v_i) {
-	li crr = 1;
-	li prc = -1;
- 
-	while (frogs[crr].id != -1) {
-		prc = crr;
-		if (pos < frogs[crr].pos)
-			crr = frogs[crr].l;
-		else
-			crr = frogs[crr].r;
+
+
+void update_frog_tree(std::multimap<li, li>::iterator i, std::multimap<li, li>& fst, std::vector<frog>& fsl) {
+	frog crn_f = fsl[i->second];
+	auto ii = std::next(i);
+
+	while (ii != fst.end()) {
+		frog next_f = fsl[ii->second];
+		if (next_f.pos + next_f.tongue <= crn_f.pos + crn_f.tongue) {
+			fst.erase(ii);
+			ii = std::next(i);
+		}
+		else if (next_f.pos <= crn_f.pos + crn_f.tongue) {
+			li tmp_i = ii->second;
+			fst.erase(ii);
+			fst.insert({ crn_f.pos + crn_f.tongue + 1, tmp_i });
+			return;
+			//ii = std::next(i);
+		}
+		else {
+			return;
+		}
 	}
-	frogs[v_i].id = id;
-	frogs[v_i].pos = pos;
-	frogs[v_i].tongue = tongue;
- 
-	if (prc != -1) {
-		if (frogs[prc].pos < pos)
-			frogs[prc].r = v_i;
-		else
-			frogs[prc].l = v_i;
+
+}
+
+
+void feast(std::multimap<li, li>& ms, std::multimap<li, li>::iterator i, std::multimap<li, li>& fst, std::vector<frog>& fsl) {
+	frog f = fsl[i->second];
+	auto to_eat = ms.lower_bound(f.pos);
+	if (to_eat != ms.end() && to_eat->first >= f.pos && to_eat->first <= f.pos + f.tongue) {
+		fsl[i->second].mos_count++;
+		fsl[i->second].tongue += to_eat->second;
+
+		ms.erase(to_eat);
+		update_frog_tree(i, fst, fsl);
+		return feast(ms, i, fst, fsl);
 	}
 }
 
-// OK!
-void find_frog(std::vector<frog>& frogs, li i, li m_pos, li& f_i) {
-	// No more frogs to check, end
-	if (frogs[i].id == -1) return;
-
-	// Check if current frog is best candidate
-	bool can_eat = frogs[i].pos <= m_pos && (frogs[i].pos + frogs[i].tongue) >= m_pos;
-	bool most_left = f_i == -1 || frogs[i].pos < frogs[f_i].pos;
-
-
-	if (can_eat && most_left) { 					// If the frog match the requisites update index and check for leftmost frogs
-		f_i = i;
-		find_frog(frogs, frogs[i].l, m_pos, f_i);
+// 30L + bacio accademico
+std::multimap<li, li>::iterator find_kermit(li x, li s, std::multimap<li, li>& fst, std::vector<frog>& fsl) {
+	auto i = fst.lower_bound(x);
+	if (i->first == x) {
+		frog f = fsl[i->second];
+		if (x >= f.pos && x <= f.pos + f.tongue) {
+			return i;
+		}
+		return fst.end();
 	}
-	else if (m_pos < frogs[i].pos) {				// If the mosq is to the left size of the current frog, check only leftmost frogs
-		find_frog(frogs, frogs[i].l, m_pos, f_i);
+	else if (i != fst.begin()) {
+		i = std::prev(i);
+		frog f = fsl[i->second];
+		if (x >= f.pos && x <= f.pos + f.tongue) {
+			return i;
+		}
+		return fst.end();
 	}
-	else {											// General case
-		find_frog(frogs, frogs[i].l, m_pos, f_i);
-		find_frog(frogs, frogs[i].r, m_pos, f_i);
-	}
+	return fst.end();
 }
 
-// void find_frog2(std::multimap<my_type, frog>& frog_tree, my_type m_pos, my_type m_s) {
-// 	auto gonna_eat = frog_tree.lower_bound(m_pos);
-// 	auto f = gonna_eat->second;
-// 	if (gonna_eat != frog_tree.end() && m_pos <= gonna_eat->second.pos + gonna_eat->second.tongue) {
-// 		gonna_eat->second.update_frog(m_s);
-// 	}
-// }
+bool sort_f_pos(frog a, frog b) { return a.pos < b.pos; }
 
-// Main
+// Mainupd
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
@@ -115,15 +101,33 @@ int main()
 	scanf("%ld", &f_n);
 	scanf("%ld", &m_n);
 
-	std::vector<frog> frogs(f_n + 1);
-	li free_pos = 1;
+	std::vector<frog> frog_list;
+	frog_list.reserve(f_n);
+
 
 	for (li i = 0; i < f_n; i++) {
 		li x, t;
 		scanf("%ld", &x);
 		scanf("%ld", &t);
-		
-		insert_frog(frogs, i, x, t, free_pos++);
+		frog_list.push_back(frog(i, x, t));
+	}
+
+	std::sort(frog_list.begin(), frog_list.end(), sort_f_pos);
+	std::multimap<li, li> frog_tree;
+
+	li prev_pos = -1;
+
+	for (li i = 0; i < f_n; i++) {
+		if (frog_list[i].pos > prev_pos) {
+			frog_tree.insert({ frog_list[i].pos, i });
+			prev_pos = frog_list[i].pos + frog_list[i].tongue;
+		}
+		else {
+			if (frog_list[i].pos + frog_list[i].tongue > prev_pos) {
+				frog_tree.insert({ prev_pos + 1,  i });
+				prev_pos = frog_list[i].pos + frog_list[i].tongue;
+			}
+		}
 	}
 
 	std::multimap<li, li> mosqs;
@@ -133,22 +137,24 @@ int main()
 		scanf("%ld", &x);
 		scanf("%ld", &s);
 
-		li f_eat = -1;
-		find_frog(frogs, 1, x, f_eat);
+		auto itr = find_kermit(x, s, frog_tree, frog_list);
 
-		if (f_eat != -1) {
-			frogs[f_eat].update_frog(s);
-			frogs[f_eat].feast(mosqs);
+		if (itr != frog_tree.end()) {
+			frog_list[itr->second].mos_count++;
+			frog_list[itr->second].tongue += s;
+
+			update_frog_tree(itr, frog_tree, frog_list);
+			feast(mosqs, itr, frog_tree, frog_list);
 		}
 		else {
 			mosqs.insert({ x, s });
 		}
 	}
 
-	sort(frogs.begin(), frogs.end(), [](frog a, frog b) { return a.id < b.id; });
+	sort(frog_list.begin(), frog_list.end(), [](frog a, frog b) { return a.id < b.id; });
 
-	for (li i = 1; i < f_n + 1; i++) {
-		printf("%ld %ld\n", frogs[i].mos_count, frogs[i].tongue);
+	for (li i = 0; i < f_n; i++) {
+		printf("%ld %ld\n", frog_list[i].mos_count, frog_list[i].tongue);
 	}
 
 	return 0;
