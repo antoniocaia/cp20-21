34abb234dc8ee874a2b3ef751de8b224cd7f4b47
Antonio Caia <antonio.caia96@gmail.com>
2021-02-07 12:45:43 +0100

    Old version

--- code/tree_and_queries.cpp
+++ code/tree_and_queries.cpp
@@ -1,82 +1,111 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
+ 
 using li = long int;
-
+ 
 struct query {
 	li root;
 	li k;
 	li ind;
-
+ 
 	query(li r, li kk, li i) {
 		root = r;
 		k = kk;
 		ind = i;
 	}
 };
-
-li num_color;
-
+ 
+ 
 struct node {
 	li color;
 	li parent;
 	std::vector<li> child;
-	//std::vector<li> color_fr;
-
 	std::vector<std::pair<li, li>> node_qs;
-
-	node(li c, li p, li n, li cn) {
+ 
+	node(li c, li p, li n, li cn, li q) {
 		color = c;
 		parent = p;
-		//node_qs.reserve();
+		//node_qs.reserve(q);
 		//child.reserve(n);
 	}
 };
-
-std::vector<li> visit(li ind, std::vector<node>& ns, std::vector<std::pair<li, li>>& s) {
+ 
+bool sort_dec(li a, li b) { return a > b; };
+bool sort_second(std::pair<li, li> a, std::pair<li, li> b) { return a.second < b.second; }
+bool sort_first(std::pair<li, li> a, std::pair<li, li> b) { return a.first > b.first; }
+ 
+std::vector<node> ns;
+std::vector<std::pair<li, li>> s;
+ 
+ 
+std::vector<li> visit(li ind, li num_color) {
 	std::vector<li> color_frq(num_color);
-
+ 
 	// Update the frquencies
-	for (int i = 0; i < ns[ind].child.size(); i++) {
-		std::vector<li> tmp = visit(ns[ind].child[i], ns, s);
+	for (li i = 0; i < ns[ind].child.size(); i++) {
+		std::vector<li> tmp_col_fr = visit(ns[ind].child[i], num_color);
 		//merge sol
-		for (int j = 0; j < tmp.size(); j++) {
-			color_frq[j] += tmp[j];
+		for (li j = 0; j < tmp_col_fr.size(); j++) {
+			color_frq[j] += tmp_col_fr[j];
 		}
 	}
 	color_frq[ns[ind].color]++;
-
-	//Solve query
-	for (int i = 0; i < ns[ind].node_qs.size(); i++) {
-		li k = ns[ind].node_qs[i].first;
-		li ti = ns[ind].node_qs[i].second;
-
+ 
+ 
+	// Process queries
+	if (ns[ind].node_qs.size() > 0) {
+		std::sort(ns[ind].node_qs.begin(), ns[ind].node_qs.end(), sort_first);
+ 
+		std::vector<li> sorted_col_freq = color_frq;
+		std::sort(sorted_col_freq.begin(), sorted_col_freq.end(), sort_dec);
+ 
 		li tot = 0;
-		for (int j = 0; j < color_frq.size(); j++) {
-			if (color_frq[j] >= k) tot++;
+		li q_index = 0;
+ 
+		li k = ns[ind].node_qs[q_index].first;
+		li ti = ns[ind].node_qs[q_index].second;
+		li j = 0;
+		while (j < sorted_col_freq.size()) {
+			if (sorted_col_freq[j] >= k) {
+				tot++;
+				j++;
+			}
+			else {
+				s.push_back(std::make_pair(tot, ti));
+				q_index++;
+				if (q_index >= ns[ind].node_qs.size()) return color_frq;
+				k = ns[ind].node_qs[q_index].first;
+				ti = ns[ind].node_qs[q_index].second;
+			}
 		}
-		s.push_back(std::make_pair(tot, ti));
+		// solver remaining query
+		for (li i = q_index; i < ns[ind].node_qs.size(); i++) {
+			li tii = ns[ind].node_qs[i].second;
+			s.push_back(std::make_pair(tot, tii));
+		}
 	}
+ 
 	return color_frq;
 }
-
-bool sort_sol(std::pair<li, li> a, std::pair<li, li> b) { return a.second < b.second; }
-
+ 
+ 
+ 
+ 
 int main() {
 	std::ios_base::sync_with_stdio(false);
-
+ 
 	// input
 	li n, q;
 	scanf("%ld", &n);
 	scanf("%ld", &q);
-
+ 
 	// remap colors
 	std::vector<li> colors(100001, -1);
 	li color_n = 0;
 	std::vector<li> mem_col(n);
-
-	for (int i = 0; i < n; i++) {
+ 
+	for (li i = 0; i < n; i++) {
 		li color;
 		scanf("%ld", &color);
 		if (colors[color] == -1) {
@@ -85,47 +114,45 @@ int main() {
 		}
 		mem_col[i] = colors[color];
 	}
-	num_color = color_n;
-
-	std::vector<node> nodes;
-	nodes.reserve(n);
-
-	for (int i = 0; i < n; i++) {
-		nodes.push_back(node(mem_col[i], -1, n, color_n));
+ 
+	ns.reserve(n);
+ 
+	for (li i = 0; i < n; i++) {
+		ns.push_back(node(mem_col[i], -1, n, color_n, q));
 	}
-
-	for (int i = 0; i < n - 1; i++) {
+ 
+	for (li i = 0; i < n - 1; i++) {
 		li p;
 		li c;
 		scanf("%ld", &p);
 		scanf("%ld", &c);
 		p -= 1;
 		c -= 1;
-
+ 
 		if (p > c) {
 			li tmp = p;
 			p = c;
 			c = tmp;
 		}
-
-		nodes[p].child.push_back(c);
-		nodes[c].parent = p;
+ 
+		ns[p].child.push_back(c);
+		ns[c].parent = p;
 	}
-
-	for (int i = 0; i < q; i++) {
+ 
+	for (li i = 0; i < q; i++) {
 		li root;
 		li k;
 		scanf("%ld", &root);
 		scanf("%ld", &k);
 		root -= 1;
-		nodes[root].node_qs.push_back(std::make_pair(k, i));
+		ns[root].node_qs.push_back(std::make_pair(k, i));
 	}
-
-	std::vector<std::pair<li, li>> solutions;
-	visit(0, nodes, solutions);
-
-	std::sort(solutions.begin(), solutions.end(), sort_sol);
-	for (int i = 0; i < solutions.size(); i++) {
-		printf("%ld\n", solutions[i].first);
+ 
+	s.reserve(q);
+	visit(0, color_n);
+ 
+	std::sort(s.begin(), s.end(), sort_second);
+	for (li i = 0; i < s.size(); i++) {
+		printf("%ld\n", s[i].first);
 	}
 }
